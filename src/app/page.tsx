9a1e4a0f1ba5380ef391b4f56bb273c09d334284
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-02-24 18:08:31 +0900

    ユーザーの現在地を取得できる機能を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,59 @@ import MapView from "@/components/MapView";
 import { useState } from "react";
 
 export default function Home() {
-  const [showDestinationSetter, setShowDestinationSetter] = useState(false);
+  const [showMapView, setShowMapView] = useState(false);
+  const [userCurrentLocation, setUserCurrentLocation] = useState<google.maps.LatLngLiteral>({
+    lat: 35.69575,
+    lng: 139.77521
+  });
 
-  const handleDestinationSetterOpen = () => {
-    setShowDestinationSetter(true);
-  };
+  const handleClick = () => {
+    handleGeolocation();
+    setShowMapView(true);
+  }
+
+  // ユーザーの現在地を取得する
+  const handleGeolocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserCurrentLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          });
+        },
+        (error) => {
+          if (error.code === error.PERMISSION_DENIED) {
+            alert('位置情報の利用がブロックされました。設定から許可してください。\n' +
+                  '（設定方法例）\n' +
+                  'Chrome: ⋮ > 設定 > プライバシーとセキュリティ > サイトの設定 > 位置情報\n' +
+                  'Safari: 設定 > プライバシー > 位置情報サービス');
+          }
+          setShowMapView(false);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 10000,
+          maximumAge: 0
+        }
+      );
+    } else {
+      alert('このブラウザは位置情報サービスをサポートしていません');
+    }
+  }
+  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold mb-8">ドライブビンゴ</h1>
-      {/* ボタンをクリックするとGoogle Mapsに遷移します */}
       <button
        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-       onClick={handleDestinationSetterOpen}
+       onClick={handleClick}
      >
       目的地を設定
      </button>
-     {showDestinationSetter && (
-      <MapView />
+     {showMapView && (
+      <MapView userCurrentLocation={userCurrentLocation} />
      )}
     </main>
   );
--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -5,15 +5,21 @@ import GoogleMapLoader from './GoogleMapLoader';
 import GoogleMapRenderer from './GoogleMapRenderrer';
 
 /**
+ * MapViewのProps
+ */
+type MapViewProps = {
+  userCurrentLocation: {
+    lat: number;
+    lng: number;
+  }
+}
+
+/**
  * GoogleMapを表示するコンポーネント
 */
-const MapView = () => {
+const MapView = ({ userCurrentLocation }: MapViewProps) => {
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
   // TODO: ユーザーの現在地を取得するロジックに変更する
-  const userCurrentLocation = {
-    lat: 35.69575,
-    lng: 139.77521,
-  }
   const zoom = 10
 
   if (!apiKey) {
