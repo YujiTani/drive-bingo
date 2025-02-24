05a0d19010437f43ecadf5a9ef81561543d68fde
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-02-24 17:20:22 +0900

    コンポーネント名をDestinationSetterからMapViewに変更し、GoogleMapを表示するロジックを更新

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -5,28 +5,29 @@ import GoogleMapLoader from './GoogleMapLoader';
 import GoogleMapRenderer from './GoogleMapRenderrer';
 
 /**
- * 目的地を設定するコンポーネント
+ * GoogleMapを表示するコンポーネント
 */
-const DestinationSetter = () => {
+const MapView = () => {
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
   // TODO: ユーザーの現在地を取得するロジックに変更する
-  const initialMapCenter = {
+  const userCurrentLocation = {
     lat: 35.69575,
     lng: 139.77521,
   }
+  const zoom = 10
 
   if (!apiKey) {
     console.error("Google Maps API Key is not set");
     return null;
   }
-
+  
   return (
     <div style={{ height: '100vh', width: '100%' }}>
       <Wrapper apiKey={apiKey} render={GoogleMapLoader} >
-        <GoogleMapRenderer center={initialMapCenter} zoom={10} />
+        <GoogleMapRenderer userCurrentLocation={userCurrentLocation} zoom={zoom} />
       </Wrapper>
     </div>
   )
 }
 
-export default DestinationSetter
+export default MapView
